f8bf15e0538fde7bc543636a9f82011d15dbb705
Саша <sasha-nechunaev@mail.ru>
2026-06-13 15:19:10 +0300

    rename bot_token to BOT_TOKEN

--- tasks/auth_telegram.py
+++ tasks/auth_telegram.py
@@ -4,8 +4,8 @@ import time
 from urllib.parse import parse_qsl
 
 
-def verify_telegram_data(bot_token, init_data, max_age_seconds=None):
-    if not bot_token or not init_data:
+def verify_telegram_data(BOT_TOKEN, init_data, max_age_seconds=None):
+    if not BOT_TOKEN or not init_data:
         return False
 
     try:
@@ -19,7 +19,7 @@ def verify_telegram_data(bot_token, init_data, max_age_seconds=None):
 
     data_check_string = "\n".join([f'{k}={v}' for k, v in sorted(vals.items())])
 
-    secret_key = hmac.new('WebAppData'.encode(), bot_token.encode(), hashlib.sha256).digest()
+    secret_key = hmac.new('WebAppData'.encode(), BOT_TOKEN.encode(), hashlib.sha256).digest()
 
     calculated_hash = hmac.new(secret_key, data_check_string.encode(), hashlib.sha256).hexdigest()
 
